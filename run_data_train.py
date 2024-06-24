2ddf8d60b3dedb38f20eac0e730b9fbc5161473d
Ri Nandiya <rnandiya@kaya1.hpc.uwa.edu.au>
2024-06-24 15:02:41 +0800

    update gitignore + done adding selective search

---
+++ run_data_train.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+from transformers import GPT2LMHeadModel
+import torchvision.models as models
+from transformers import PreTrainedModel, ViTModel
+from transformers.modeling_outputs import ModelOutput
+from dataclasses import dataclass
+from transformers import ViTConfig
+
+
